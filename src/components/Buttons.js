73e7f94aab571d2921d3a9f3e7a7c7246dbe2e27
TURKCELL\tcceroglu <cem.eroglu@turkcell.com.tr>
2021-03-26 15:23:23 +0300

    Dispatch added.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import {useDispatch} from 'react-redux';
+import {arttır, azalt} from '../store/actions/counter'
 
 const Buttons=()=>{
+    const dispatch = useDispatch();
     return(
         <div>
-        <button>-</button>
-        <button>+</button>
+        <button onClick={()=>dispatch(azalt())}>-</button>
+        <button onClick={()=>dispatch(arttır())}>+</button>
         </div>
     )
 }
